06a66697b9d71a0359daf575348a2f2f904e3e10
Diana Ivanova <din.ivanova13@gmail.com>
2020-01-13 14:19:25 +0300

    fix: change desctiption example

--- arrays/chunk.js
+++ arrays/chunk.js
@@ -7,8 +7,8 @@
  * @return {array} Returns the new array of chunks
  *
  * @example:
- * _.chunk(['a', 'b', 'c', 'd'], 2)     => [['a', 'b'], ['c', 'd']]
- * _.chunk(['a', 'b', 'c', 'd'], 3)     => [['a', 'b', 'c'], ['d']]
+ * chunk(['a', 'b', 'c', 'd'], 2)     => [['a', 'b'], ['c', 'd']]
+ * chunk(['a', 'b', 'c', 'd'], 3)     => [['a', 'b', 'c'], ['d']]
 */
 
 function chunk(arr, num) {
