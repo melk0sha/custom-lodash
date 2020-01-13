bb8a213bddfd58051fe82a9f104a37a9eb3d5a36
Diana Ivanova <din.ivanova13@gmail.com>
2020-01-13 14:30:46 +0300

    feat: add drop to arrays

---
+++ arrays/drop.js
@@ -0,0 +1,17 @@
+/**
+ * Creates a slice of array with n elements dropped from the beginning.
+ * 
+ * @param {array} The array to query
+ * @param {number} The number of elements to drop
+ * @return {array} Returns the slice of array
+ *
+ * @example:
+ * drop([1, 2, 3])          => [2, 3]
+ * drop([1, 2, 3], 2)       => [3]
+ * drop([1, 2, 3], 5)       => []
+ * drop([1, 2, 3], 0)       => [1, 2, 3]
+*/
+
+function drop(arr, num = 1) {
+    return arr.filter((el, i) => i >= num);
+}
